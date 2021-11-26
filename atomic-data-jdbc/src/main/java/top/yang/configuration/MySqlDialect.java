bff008b59927d53f4410374b2331725d87877139
PrideYang <pride.yang@outlook.com>
2021-11-26 13:35:45 +0800

    add

--- atomic-data-jdbc/src/main/java/top/yang/configuration/MySqlDialect.java
+++ atomic-data-jdbc/src/main/java/top/yang/configuration/MySqlDialect.java
@@ -10,135 +10,138 @@ import org.springframework.data.relational.core.sql.LockOptions;
 import org.springframework.util.Assert;
 import top.yang.configuration.MySqlIdentifierProcessing.LetterCasing;
 
+/**
+ * @author pride
+ */
 public class MySqlDialect extends AbstractDialect {
 
-  /**
-   * MySQL defaults for {@link IdentifierProcessing}.
-   */
-  public static final IdentifierProcessing MYSQL_IDENTIFIER_PROCESSING = new MySqlIdentifierProcessing(new Quoting("`"),
-      LetterCasing.UNDERLINE_CASE);
-  /**
-   * Singleton instance.
-   */
-  public static final MySqlDialect INSTANCE = new MySqlDialect();
+    /**
+     * MySQL defaults for {@link IdentifierProcessing}.
+     */
+    public static final IdentifierProcessing MYSQL_IDENTIFIER_PROCESSING = new MySqlIdentifierProcessing(new Quoting("`"),
+            LetterCasing.UNDERLINE_CASE);
+    /**
+     * Singleton instance.
+     */
+    public static final MySqlDialect INSTANCE = new MySqlDialect();
 
-  private final IdentifierProcessing identifierProcessing;
+    private final IdentifierProcessing identifierProcessing;
 
-  protected MySqlDialect() {
-    this(MYSQL_IDENTIFIER_PROCESSING);
-  }
+    protected MySqlDialect() {
+        this(MYSQL_IDENTIFIER_PROCESSING);
+    }
 
-  /**
-   * Creates a new {@link org.springframework.data.relational.core.dialect.MySqlDialect} given {@link IdentifierProcessing}.
-   *
-   * @param identifierProcessing must not be null.
-   * @since 2.0
-   */
-  public MySqlDialect(IdentifierProcessing identifierProcessing) {
+    /**
+     * Creates a new {@link org.springframework.data.relational.core.dialect.MySqlDialect} given {@link IdentifierProcessing}.
+     *
+     * @param identifierProcessing must not be null.
+     * @since 2.0
+     */
+    public MySqlDialect(IdentifierProcessing identifierProcessing) {
 
-    Assert.notNull(identifierProcessing, "IdentifierProcessing must not be null");
+        Assert.notNull(identifierProcessing, "IdentifierProcessing must not be null");
 
-    this.identifierProcessing = identifierProcessing;
-  }
+        this.identifierProcessing = identifierProcessing;
+    }
 
-  private static final LimitClause LIMIT_CLAUSE = new LimitClause() {
+    private static final LimitClause LIMIT_CLAUSE = new LimitClause() {
+
+        /*
+         * (non-Javadoc)
+         * @see org.springframework.data.relational.core.dialect.LimitClause#getLimit(long)
+         */
+        @Override
+        public String getLimit(long limit) {
+            return "LIMIT " + limit;
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see org.springframework.data.relational.core.dialect.LimitClause#getOffset(long)
+         */
+        @Override
+        public String getOffset(long offset) {
+            // Ugly but the official workaround for offset without limit
+            // see: https://stackoverflow.com/a/271650
+            return String.format("LIMIT %d, 18446744073709551615", offset);
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see org.springframework.data.relational.core.dialect.LimitClause#getClause(long, long)
+         */
+        @Override
+        public String getLimitOffset(long limit, long offset) {
+
+            // LIMIT {[offset,] row_count}
+            return String.format("LIMIT %s, %s", offset, limit);
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see org.springframework.data.relational.core.dialect.LimitClause#getClausePosition()
+         */
+        @Override
+        public Position getClausePosition() {
+            return Position.AFTER_ORDER_BY;
+        }
+    };
+
+    private static final LockClause LOCK_CLAUSE = new LockClause() {
+
+        /*
+         * (non-Javadoc)
+         * @see org.springframework.data.relational.core.dialect.LockClause#getLock(LockOptions)
+         */
+        @Override
+        public String getLock(LockOptions lockOptions) {
+            switch (lockOptions.getLockMode()) {
+
+                case PESSIMISTIC_WRITE:
+                    return "FOR UPDATE";
+
+                case PESSIMISTIC_READ:
+                    return "LOCK IN SHARE MODE";
+
+                default:
+                    return "";
+            }
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see org.springframework.data.relational.core.dialect.LockClause#getClausePosition()
+         */
+        @Override
+        public Position getClausePosition() {
+            return Position.AFTER_ORDER_BY;
+        }
+    };
 
     /*
      * (non-Javadoc)
-     * @see org.springframework.data.relational.core.dialect.LimitClause#getLimit(long)
+     * @see org.springframework.data.relational.core.dialect.Dialect#limit()
      */
     @Override
-    public String getLimit(long limit) {
-      return "LIMIT " + limit;
+    public LimitClause limit() {
+        return LIMIT_CLAUSE;
     }
 
     /*
      * (non-Javadoc)
-     * @see org.springframework.data.relational.core.dialect.LimitClause#getOffset(long)
+     * @see org.springframework.data.relational.core.dialect.Dialect#lock()
      */
     @Override
-    public String getOffset(long offset) {
-      // Ugly but the official workaround for offset without limit
-      // see: https://stackoverflow.com/a/271650
-      return String.format("LIMIT %d, 18446744073709551615", offset);
+    public LockClause lock() {
+        return LOCK_CLAUSE;
     }
 
     /*
      * (non-Javadoc)
-     * @see org.springframework.data.relational.core.dialect.LimitClause#getClause(long, long)
+     * @see org.springframework.data.relational.core.dialect.Dialect#getIdentifierProcessing()
      */
     @Override
-    public String getLimitOffset(long limit, long offset) {
-
-      // LIMIT {[offset,] row_count}
-      return String.format("LIMIT %s, %s", offset, limit);
+    public IdentifierProcessing getIdentifierProcessing() {
+        return identifierProcessing;
     }
-
-    /*
-     * (non-Javadoc)
-     * @see org.springframework.data.relational.core.dialect.LimitClause#getClausePosition()
-     */
-    @Override
-    public Position getClausePosition() {
-      return Position.AFTER_ORDER_BY;
-    }
-  };
-
-  private static final LockClause LOCK_CLAUSE = new LockClause() {
-
-    /*
-     * (non-Javadoc)
-     * @see org.springframework.data.relational.core.dialect.LockClause#getLock(LockOptions)
-     */
-    @Override
-    public String getLock(LockOptions lockOptions) {
-      switch (lockOptions.getLockMode()) {
-
-        case PESSIMISTIC_WRITE:
-          return "FOR UPDATE";
-
-        case PESSIMISTIC_READ:
-          return "LOCK IN SHARE MODE";
-
-        default:
-          return "";
-      }
-    }
-
-    /*
-     * (non-Javadoc)
-     * @see org.springframework.data.relational.core.dialect.LockClause#getClausePosition()
-     */
-    @Override
-    public Position getClausePosition() {
-      return Position.AFTER_ORDER_BY;
-    }
-  };
-
-  /*
-   * (non-Javadoc)
-   * @see org.springframework.data.relational.core.dialect.Dialect#limit()
-   */
-  @Override
-  public LimitClause limit() {
-    return LIMIT_CLAUSE;
-  }
-
-  /*
-   * (non-Javadoc)
-   * @see org.springframework.data.relational.core.dialect.Dialect#lock()
-   */
-  @Override
-  public LockClause lock() {
-    return LOCK_CLAUSE;
-  }
-
-  /*
-   * (non-Javadoc)
-   * @see org.springframework.data.relational.core.dialect.Dialect#getIdentifierProcessing()
-   */
-  @Override
-  public IdentifierProcessing getIdentifierProcessing() {
-    return identifierProcessing;
-  }
 }
--- atomic-data-jdbc/src/main/java/top/yang/configuration/MySqlIdentifierProcessing.java
+++ atomic-data-jdbc/src/main/java/top/yang/configuration/MySqlIdentifierProcessing.java
@@ -2,58 +2,76 @@ package top.yang.configuration;
 
 
 import org.springframework.data.relational.core.sql.IdentifierProcessing;
-import top.yang.string.StringUtils;
 
 class MySqlIdentifierProcessing implements IdentifierProcessing {
 
-  private final Quoting quoting;
-  private final LetterCasing letterCasing;
+    private final Quoting quoting;
+    private final LetterCasing letterCasing;
 
-  public MySqlIdentifierProcessing(Quoting quoting, LetterCasing letterCasing) {
-    this.quoting = quoting;
-    this.letterCasing = letterCasing;
-  }
+    public MySqlIdentifierProcessing(Quoting quoting, LetterCasing letterCasing) {
+        this.quoting = quoting;
+        this.letterCasing = letterCasing;
+    }
 
-  @Override
-  public String quote(String identifier) {
-    return quoting.apply(identifier);
-  }
+    @Override
+    public String quote(String identifier) {
+        return quoting.apply(identifier);
+    }
 
-  @Override
-  public String standardizeLetterCase(String identifier) {
-    return letterCasing.apply(identifier);
-  }
+    @Override
+    public String standardizeLetterCase(String identifier) {
+        return letterCasing.apply(identifier);
+    }
 
-  enum LetterCasing {
+    enum LetterCasing {
 
-    UPPER_CASE {
-      @Override
-      public String apply(String identifier) {
-        return identifier.toUpperCase();
-      }
-    },
+        UPPER_CASE {
+            @Override
+            public String apply(String identifier) {
+                return identifier.toUpperCase();
+            }
+        },
 
-    LOWER_CASE {
-      @Override
-      public String apply(String identifier) {
-        return identifier.toLowerCase();
-      }
-    },
+        LOWER_CASE {
+            @Override
+            public String apply(String identifier) {
+                return identifier.toLowerCase();
+            }
+        },
 
-    UNDERLINE_CASE {
-      @Override
-      public String apply(String identifier) {
-        return StringUtils.convertToUnderline(identifier);
-      }
-    },
-    AS_IS {
-      @Override
-      public String apply(String identifier) {
-        return identifier;
-      }
-    };
+        UNDERLINE_CASE {
+            @Override
+            public String apply(String identifier) {
+                return convertToUnderline(identifier);
+            }
+        },
+        AS_IS {
+            @Override
+            public String apply(String identifier) {
+                return identifier;
+            }
+        };
 
-    abstract String apply(String identifier);
-  }
+        abstract String apply(String identifier);
+    }
 
+    /**
+     * @param name 小驼峰命名法
+     * @return 下划线格式
+     */
+    private static String convertToUnderline(String name) {
+        StringBuilder builder = new StringBuilder(name.replace('.', '_'));
+        for (int i = 1; i < builder.length() - 1; i++) {
+            if (isUnderscoreRequired(builder.charAt(i - 1), builder.charAt(i),
+                    builder.charAt(i + 1))) {
+                builder.insert(i++, '_');
+            }
+        }
+        return builder.toString().toUpperCase();
+    }
+
+    private static boolean isUnderscoreRequired(char before, char current, char after) {
+        return Character.isLowerCase(before) && Character.isUpperCase(current)
+                && Character.isLowerCase(after);
+    }
 }
