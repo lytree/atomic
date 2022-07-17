5a52ca57b89d23fca7654f21ecc92ee3807820e5
PrideYang <hi.yangzh@outlook.com>
2022-07-17 09:36:21 +0800

    update redis

--- atomic-data-redis/src/main/java/top/yang/redis/configure/RedisConfig.java
+++ atomic-data-redis/src/main/java/top/yang/redis/configure/RedisConfig.java
@@ -4,8 +4,12 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import io.lettuce.core.RedisClient;
 import java.time.Duration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
@@ -21,48 +25,50 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 public class RedisConfig extends CachingConfigurerSupport {
 
-  @Bean
-  @SuppressWarnings(value = {"unchecked", "rawtypes"})
-  public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-    RedisTemplate<Object, Object> template = new RedisTemplate<>();
-    template.setConnectionFactory(connectionFactory);
+    @Bean
+    @SuppressWarnings(value = {"unchecked", "rawtypes"})
+    @ConditionalOnClass({RedisClient.class})
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
 
-    Jackson2JsonRedisSerializer serializer = getRedisSerializer();
+        Jackson2JsonRedisSerializer serializer = getRedisSerializer();
 
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-    mapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
-    serializer.setObjectMapper(mapper);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        serializer.setObjectMapper(mapper);
 
-    // 使用StringRedisSerializer来序列化和反序列化redis的key值
-    template.setKeySerializer(new StringRedisSerializer());
-    template.setValueSerializer(serializer);
+        // 使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
 
-    // Hash的key也采用StringRedisSerializer的序列化方式
-    template.setHashKeySerializer(new StringRedisSerializer());
-    template.setHashValueSerializer(serializer);
+        // Hash的key也采用StringRedisSerializer的序列化方式
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
 
-    template.setDefaultSerializer(RedisSerializer.json());
-    template.afterPropertiesSet();
-    return template;
-  }
+        template.setDefaultSerializer(RedisSerializer.json());
+        template.afterPropertiesSet();
+        return template;
+    }
 
 
-  @Bean
-  public CacheManager redisCacheManager(RedisConnectionFactory connectionFactory) {
-    RedisSerializationContext.SerializationPair serializationPair =
-        RedisSerializationContext.SerializationPair.fromSerializer(getRedisSerializer());
-    RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-        .entryTtl(Duration.ofSeconds(30))
-        .serializeValuesWith(serializationPair);
-    return RedisCacheManager
-        .builder(RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory))
-        .cacheDefaults(redisCacheConfiguration).build();
-  }
+    @Bean
+    @ConditionalOnClass({RedisClient.class})
+    public CacheManager redisCacheManager(RedisConnectionFactory connectionFactory) {
+        RedisSerializationContext.SerializationPair serializationPair =
+                RedisSerializationContext.SerializationPair.fromSerializer(getRedisSerializer());
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(30))
+                .serializeValuesWith(serializationPair);
+        return RedisCacheManager
+                .builder(RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory))
+                .cacheDefaults(redisCacheConfiguration).build();
+    }
 
-  private Jackson2JsonRedisSerializer<Object> getRedisSerializer() {
-    return new Jackson2JsonRedisSerializer(Object.class);
-  }
+    private Jackson2JsonRedisSerializer<Object> getRedisSerializer() {
+        return new Jackson2JsonRedisSerializer(Object.class);
+    }
 
 
 }
