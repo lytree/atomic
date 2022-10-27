5d1cdd3cb9e19c1c021d942f4bbac6ce188899a9
PrideYang <hi.yangzh@outlook.com>
2022-10-27 13:46:25 +0800

    删除多余注释

--- atomic-core/src/main/java/top/lytree/bean/BeanDesc.java
+++ atomic-core/src/main/java/top/lytree/bean/BeanDesc.java
@@ -11,20 +11,7 @@ import top.lytree.base.Assert;
 import top.lytree.lang.BooleanUtils;
 import top.lytree.lang.StringUtils;
 
-/**
- * Bean信息描述做为BeanInfo替代方案，此对象持有JavaBean中的setters和getters等相关信息描述<br>
- * 查找Getter和Setter方法时会：
- *
- * <pre>
- * 1. 忽略字段和方法名的大小写
- * 2. Getter查找getXXX、isXXX、getIsXXX
- * 3. Setter查找setXXX、setIsXXX
- * 4. Setter忽略参数值与字段值不匹配的情况，因此有多个参数类型的重载时，会调用首次匹配的
- * </pre>
- *
- * @author looly
- * @since 3.1.2
- */
+
 public class BeanDesc implements Serializable {
 
     private static final long serialVersionUID = 1L;
