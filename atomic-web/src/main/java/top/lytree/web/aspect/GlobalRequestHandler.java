30cc1f6ff8e902f7f782a75186df9dca918b2da4
lytree <hi.yangzh@outlook.com>
2023-06-11 10:06:13 +0800

    调整工具类

---
+++ atomic-web/src/main/java/top/lytree/web/aspect/GlobalRequestHandler.java
@@ -0,0 +1,38 @@
+package top.lytree.web.aspect;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdvice;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+/**
+ * @author Y
+ * 请求参数处理
+ */
+@ControllerAdvice(basePackages = "top.yang")
+public class GlobalRequestHandler implements RequestBodyAdvice {
+
+    @Override
+    public boolean supports(MethodParameter methodParameter, Type type, Class<? extends HttpMessageConverter<?>> aClass) {
+        return false;
+    }
+
+    @Override
+    public HttpInputMessage beforeBodyRead(HttpInputMessage httpInputMessage, MethodParameter methodParameter, Type type, Class<? extends HttpMessageConverter<?>> aClass) throws IOException {
+        return null;
+    }
+
+    @Override
+    public Object afterBodyRead(Object o, HttpInputMessage httpInputMessage, MethodParameter methodParameter, Type type, Class<? extends HttpMessageConverter<?>> aClass) {
+        return null;
+    }
+
+    @Override
+    public Object handleEmptyBody(Object o, HttpInputMessage httpInputMessage, MethodParameter methodParameter, Type type, Class<? extends HttpMessageConverter<?>> aClass) {
+        return null;
+    }
+}
